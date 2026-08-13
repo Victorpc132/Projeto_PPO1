240bf59f7d95624e663de4bb294786fd79b3a34a
Victorpc132 <vmano9251@gmail.com>
2026-08-13 02:53:25 -0300

    Conecta backend ao PostgreSQL

--- Matchmaking_Games/backend/server.ts
+++ Matchmaking_Games/backend/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import {pool} from './db';
+
 
 const app = express();
 
@@ -14,6 +16,26 @@ app.post('/ola', (req, res) => {
         });
 });
 
+app.get("/teste-banco", async (req, res) => {
+    try{
+        const resultado = await pool.query("SELECT NOW()");
+
+        res.json({mensagem: "Banco conectado com sucesso!",
+            horario: resultado.rows[0].now
+
+        });
+    }catch (erro) {
+        console.error(erro);
+
+
+        res.status(500).json({
+            mensagem: "Erro ao conectar com o banco"
+        });
+    }    
+});
+
+console.log("tipo da senha: ", typeof process.env.DB_PASSWORD);
+console.log("Banco:" , process.env.DB_DATABASE);
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
